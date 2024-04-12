0131f02e7d39ff5ae147b57cac9eaac5bb908ced
tubao2711 <bao0975448317@gmail.com>
2024-04-12 13:30:14 +0700

    starter

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,13 @@
+import { Error, Loader, SongCard } from "../components";
+import {genres} from "..assets/constant.js"
+
+const Discover = () => {return (
+  console.log(genres)
+  <div className ="flex flex-col"> 
+    <div className="w-full flex justify-between items-center sm:flex-row">
+
+    </div> 
+  </div>
+)};
+
+export default Discover;
